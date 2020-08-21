566b1326bee95725c531708bbc8fe011b8c1f319
racheltara <racheltara.ambrosio@gmail.com>
2020-08-21 03:23:00 +0000

    add overall layout

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,14 @@
 get '/' do
   File.read(File.join('app/views', 'index.html'))
 end
+get '/' do
+  File.read(File.join('app/views', 'index.html'))
+end
+
+
+
+
+
+get '/' do
+  File.read(File.join('app/views', 'index.html'))
+end
